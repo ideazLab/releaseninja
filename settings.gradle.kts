67c75450f3046056fa934180c13dcbc1bdcfe04c
justin emina <justin.emina@ideazlab.co.uk>
2025-09-05 07:10:12 +0100

    removing gradle

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "releaseninja"
-
-include("artifactory", "common", "github", "nexus", "web")
-
-project(":common").projectDir = file("./modules/common")
-project(":artifactory").projectDir = file("./modules/amt/artifactory")
-project(":nexus").projectDir = file("./modules/amt/nexus")
-project(":github").projectDir = file("./modules/scm/github")
